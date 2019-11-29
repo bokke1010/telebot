4473038a0de05a84eed2444fe57e5f83cbd6d82f
Bokke <bokke1010@gmail.com>
2019-11-29 15:41:12 +0100

    Remove private key from public code

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 
 const TeleBot = require('telebot');
+const token = require('./token.js');
 
-const bot = new TeleBot({
-    token: '974224497:AAFRZ-Nx-FeEGTf1mI1hOGq30d303er98f4' // Telegram Bot API token.
-});
+const bot = new TeleBot(token);
 
-bot.on('/start', function (msg) {
-  return bot.sendMessage(msg.from.id, "Hello world!");
-});
+function sendHello(msg) {
+  return bot.sendMessage(msg.from.id, "Hello! I'm unique_test_bot", {replyToMessage: msg.message_id, notification: false});
+}
+
+function stopServer(msg) {
+  console.log("stopping bot after user input");
+  bot.sendMessage(msg.from.id, "stopping bot after user input");
+  setTimeout(process.exit, 1000);
+}
+
+bot.on('/start', sendHello);
+bot.on('/stop', stopServer);
+bot.on('/logo', (msg) => bot.sendPhoto(msg.from.id, "logo original 400x400.png"))
 
 bot.start();
